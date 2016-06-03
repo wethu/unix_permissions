80052aea8c32f02e705fead0a69d84eaab01a279
Ellis Gray <ellisgray2012@gmail.com>
2016-06-03 21:57:11 +1000

    start category tree

---
+++ lib/category_tree.rb
@@ -0,0 +1,10 @@
+class CategoryTree
+  extend Forwardable
+  def add_category(category, parent)
+
+  end
+
+  def get_children(category)
+
+  end
+end
---
+++ spec/lib/category_tree_spec.rb
@@ -0,0 +1,6 @@
+require "spec_helper"
+require_relative "../../lib/category_tree"
+
+RSpec.describe CategoryTree do
+
+end
