2cb170bacfba3bce027a084399b445d7bcbcbf38
Ellis Gray <ellisgray2012@gmail.com>
2016-06-03 22:33:10 +1000

    done category

--- lib/category_tree.rb
+++ lib/category_tree.rb
@@ -1,10 +1,18 @@
 class CategoryTree
   extend Forwardable
   def add_category(category, parent)
-
+    fail ArgumentError, "#{parent} is already taken!" if table.values.flatten.include?(category)
+    table[parent] ||= []
+    table[parent] << category
   end
 
   def get_children(category)
+    table[category]
+  end
 
+  private
+
+  def table
+    @table ||= {nil => []}
   end
 end
--- spec/lib/category_tree_spec.rb
+++ spec/lib/category_tree_spec.rb
@@ -2,5 +2,19 @@ require "spec_helper"
 require_relative "../../lib/category_tree"
 
 RSpec.describe CategoryTree do
+  subject { described_class.new }
 
+  it "adds multiple categories" do
+    subject.add_category("A", nil)
+    subject.add_category("B", "A")
+    subject.add_category("C", "A")
+    expect(subject.get_children('A').join(',')).to eq "B,C"
+  end
+
+  it "raises ArgumentError when category is already taken" do
+    expect {
+      subject.add_category("A", "B")
+      subject.add_category("A", "C")
+    }.to raise_error ArgumentError
+  end
 end
